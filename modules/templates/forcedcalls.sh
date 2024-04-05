f0f7abce4507ec276cc0c80583db309ef1daa2dd
RoanKanninga <roankanninga@gmail.com>
2024-04-05 11:36:10 +0200

    fixing header in concordancecheck vcf and fixing trendanalysis output

--- modules/templates/forcedcalls.sh
+++ modules/templates/forcedcalls.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 set -o pipefail
 set -eu
@@ -16,7 +16,10 @@ if [[ -e "!{samples.combinedIdentifier}.bam" ]]
 then
 	bcftools mpileup \
 	-Ou -f "${fasta}" "!{samples.combinedIdentifier}.bam" -R "!{params.dataDir}/UMCG/concordanceCheckSnps_!{samples.build}.bed" \
-	| bcftools call -m -Ob -o "!{samples.externalSampleID}.concordanceCheckCalls.vcf"
+	| bcftools call -m -Ob -o "!{samples.externalSampleID}.concordanceCheckCalls.tmp.vcf"
+
+	echo -e "!{samples.combinedIdentifier}" "!{samples.externalSampleID}" > "!{samples.externalSampleID}.newVCFHeader.txt"
+	bcftools reheader -s "!{samples.externalSampleID}.newVCFHeader.txt" "!{samples.externalSampleID}.concordanceCheckCalls.tmp.vcf" > "!{samples.externalSampleID}.concordanceCheckCalls.vcf"
 else
 	echo "The !{samples.combinedIdentifier}.bam does not exist"
 fi
