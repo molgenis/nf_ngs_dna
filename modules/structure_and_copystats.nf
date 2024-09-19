5bf48428a46110ca36d31efb4520e59de5fc4cca
RoanKanninga <roankanninga@gmail.com>
2024-09-19 14:15:36 +0200

    copying files to other locations

--- modules/structure_and_copystats.nf
+++ modules/structure_and_copystats.nf
@@ -8,7 +8,7 @@ output:
    
   shell:
   '''
-    mkdir -m 2770 -p !{samples.projectResultsDir}/{alignment,qc,coverage,concordanceCheckSnps,variants/{gVCF,sv,cnv}}
+    mkdir -m 2770 -p !{samples.projectResultsDir}/{alignment,qc,coverage,concordanceCheckSnps,variants/{gVCF,sv,cnv,additional_analysis}}
     rsync -av "!{samples.analysisFolder}/stats.tsv" "!{samples.projectResultsDir}/qc/"
     head -1 "!{samples.projectResultsDir}/qc/stats.tsv" > "!{samples.projectResultsDir}/qc/statsRenamed.tsv"
     rsync -av "!{params.samplesheet}" "!{samples.projectResultsDir}/"
--- modules/templates/preprocess.sh
+++ modules/templates/preprocess.sh
@@ -9,6 +9,8 @@ bedfile=!{params.dataDir}/!{samples.capturingKit}/human_g1k_v37/captured.merged.
 
 grep "!{samples.combinedIdentifier}" "!{samples.projectResultsDir}/qc/stats.tsv" | perl -p -e "s|!{samples.combinedIdentifier}|!{samples.externalSampleID}|" >>  "!{samples.projectResultsDir}/qc/statsRenamed.tsv"
 
+perl -p -e "s|!{samples.combinedIdentifier}|!{samples.externalSampleID}|g" "!{samples.externalSampleID}.cnv.igv_session.xml" > "!{samples.projectResultsDir}/qc/!{samples.externalSampleID}.cnv.igv_session.xml"
+
 if [[ "!{samples.build}" == "GRCh38" ]]
 then
     bedfile="!{params.bedfile_GRCh38}"
@@ -19,54 +21,94 @@ bgzip -c -f "!{samples.externalSampleID}.variant.calls.genotyped.vcf" > "!{sampl
 tabix -p vcf "!{samples.externalSampleID}.variant.calls.genotyped.vcf.gz"
 rsync -Lv "!{samples.externalSampleID}.variant.calls.genotyped.vcf.gz"* "!{samples.projectResultsDir}/variants/"
 
+#
+## gVCF
+#
 if [[ -e "!{samples.externalSampleID}.hard-filtered.gvcf.gz" ]]
 then
-    rename ".gvcf.gz" ".g.vcf.gz" "!{samples.externalSampleID}.hard-filtered.gvcf.gz"*
-    rsync -Lv "!{samples.externalSampleID}.hard-filtered.g.vcf.gz"* "!{samples.projectResultsDir}/variants/gVCF/"
+	rename ".gvcf.gz" ".g.vcf.gz" "!{samples.externalSampleID}.hard-filtered.gvcf.gz"*
+	rsync -Lv "!{samples.externalSampleID}.hard-filtered.g.vcf.gz"* "!{samples.projectResultsDir}/variants/gVCF/"
 fi
-
+#
+## alignment
+#
 if [[ -e "!{samples.externalSampleID}.bam" ]]
 then
-    for i in "!{samples.externalSampleID}.bam"*
-    do  
-        mv $(readlink ${i}) "!{samples.projectResultsDir}/alignment/"
-    done
-    rename "!{samples.combinedIdentifier}" "!{samples.externalSampleID}" "!{samples.projectResultsDir}/alignment/"*
+	for i in "!{samples.externalSampleID}.bam"*
+	do  
+		mv $(readlink ${i}) "!{samples.projectResultsDir}/alignment/"
+	done
+	rename "!{samples.combinedIdentifier}" "!{samples.externalSampleID}" "!{samples.projectResultsDir}/alignment/"*
 fi
+
+#
+## sv
+#
 if [[ -e "!{samples.externalSampleID}.sv.vcf.gz" ]]
 then
-    rsync -Lv "!{samples.externalSampleID}.sv.vcf.gz"* "!{samples.projectResultsDir}/variants/sv/"
+	rsync -Lv "!{samples.externalSampleID}.sv.vcf.gz"* "!{samples.projectResultsDir}/variants/sv/"
 fi
+
+#
+## cnv
+#
 if [[ -e "!{samples.externalSampleID}.cnv.vcf.gz" ]]
 then
-    rsync -Lv "!{samples.externalSampleID}"*cnv* "!{samples.projectResultsDir}/variants/cnv/"
+	rsync -Lv "!{samples.externalSampleID}"*cnv* "!{samples.projectResultsDir}/variants/cnv/"
 fi 
-if [[ -e "!{samples.externalSampleID}.html" ]]
+if [[ -e "!{samples.externalSampleID}.target.counts.gz" ]]
 then
-    rsync -Lv "!{samples.externalSampleID}"*.{bed,json,html} "!{samples.projectResultsDir}/qc/"
+	rsync -Lv "!{samples.externalSampleID}"*target.counts* "!{samples.projectResultsDir}/variants/cnv/"
+fi
+if [[ -e "!{samples.externalSampleID}.tn.tsv.gz" ]]
+then
+	rsync -Lv "!{samples.externalSampleID}"*.tn* "!{samples.projectResultsDir}/variants/cnv/"
 fi
 if [[ -e "!{samples.externalSampleID}.seg" ]]
 then
-    rsync -Lv "!{samples.externalSampleID}"*seg* "!{samples.projectResultsDir}/qc/"
+	rsync -Lv "!{samples.externalSampleID}"*.seg* "!{samples.projectResultsDir}/variants/cnv/"
 fi
-if [[ -e "!{samples.externalSampleID}.target.counts.gz" ]]
+
+#
+## variants
+#
+if [[ -e "!{samples.externalSampleID}.roh.bed" ]]
 then
-    rsync -Lv "!{samples.externalSampleID}"*target.counts* "!{samples.projectResultsDir}/qc/"
+	rsync -Lv "!{samples.externalSampleID}.roh.bed" "!{samples.projectResultsDir}/variants/"
 fi
-if [[ -e "!{samples.externalSampleID}.tn.bw" ]]
+if [[ -e "!{samples.externalSampleID}.hard-filtered.baf.bw" ]]
 then
-    rsync -Lv "!{samples.externalSampleID}"*tn.tsv.gz "!{samples.projectResultsDir}/qc/"
-		rsync -Lv "!{samples.externalSampleID}"*.bw "!{samples.projectResultsDir}/qc/"
-fi
-if [[ -e "!{samples.externalSampleID}.improper.pairs.bw" ]]
-then
-    rsync -Lv "!{samples.externalSampleID}.improper.pairs.bw" "!{samples.projectResultsDir}/qc/"
+	rsync -Lv "!{samples.externalSampleID}.hard-filtered.baf.bw" "!{samples.projectResultsDir}/variants/"
 fi
 if [[ -e "!{samples.externalSampleID}.ploidy.vcf.gz" ]]
 then
-    rsync -Lv "!{samples.externalSampleID}.ploidy.vcf.gz"* "!{samples.projectResultsDir}/variants/"
+	rsync -Lv "!{samples.externalSampleID}.ploidy.vcf.gz"* "!{samples.projectResultsDir}/variants/"
+fi
+
+#
+## qc
+#
+if [[ -e "!{samples.externalSampleID}.html" ]]
+then
+	rsync -Lv "!{samples.externalSampleID}"*.{json,html} "!{samples.projectResultsDir}/qc/"
+fi
+if [[ -e "!{samples.externalSampleID}.improper.pairs.bw" ]]
+then
+	rsync -Lv "!{samples.externalSampleID}.improper.pairs.bw" "!{samples.projectResultsDir}/qc/"
 fi
 if [[ -e "sv" ]]
 then
-    rsync -Lv "sv" "!{samples.projectResultsDir}/qc/sv_!{samples.externalSampleID}"
+	rsync -Lv "sv" "!{samples.projectResultsDir}/qc/sv_!{samples.externalSampleID}"
 fi
+
+#
+## additional_analysis
+#
+if [[ -e "!{samples.externalSampleID}.smn.tsv" ]]
+then
+	rsync -Lv "!{samples.externalSampleID}.smn.tsv"* "!{samples.projectResultsDir}/variants/additional_analysis/"
+	rsync -Lv "!{samples.externalSampleID}.gba.tsv"* "!{samples.projectResultsDir}/variants/additional_analysis/"
+	rsync -Lv "!{samples.externalSampleID}.repeats.vcf"* "!{samples.projectResultsDir}/variants/additional_analysis/"
+fi
+
+
