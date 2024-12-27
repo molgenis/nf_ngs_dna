d219cb76047774703988ea9081394528c410a4cc
RoanKanninga <roankanninga@gmail.com>
2024-12-27 15:53:12 +0100

    added extra regions for concordancecheck calls

--- test/autotest.sh
+++ test/autotest.sh
@@ -8,11 +8,12 @@ pipeline='nf_ngs_dna'
 
 TMPDIRECTORY='/groups/umcg-gst/tmp07'
 GS_RUN='105856-001'
+PROJECT="GS_001-WGS_v1"
 WORKDIR="${TMPDIRECTORY}/tmp/${pipeline}/betaAutotest/"
-
+TEMP="${WORKDIR}/temp"
 ## cleanup data to get new data
 echo "cleaning up.."
-rm -rvf "${TMPDIRECTORY}/${GS_RUN}/" "${TMPDIRECTORY}/Samplesheets/GS_001A-WGS_v1.csv" "${TMPDIRECTORY}/Samplesheets/NGS_DNA/GS_001-WGS_v1.csv" "${TMPDIRECTORY}/projects/NGS_DNA/GS_001-WGS_v1"
+rm -rvf "${TMPDIRECTORY}/${GS_RUN}/" "${TMPDIRECTORY}/Samplesheets/GS_001A-WGS_v1.csv" "${TMPDIRECTORY}/Samplesheets/NGS_DNA/${PROJECT}.csv" "${TMPDIRECTORY}/projects/NGS_DNA/${PROJECT}" "${TMPDIRECTORY}/logs/${PROJECT}"
 
 ## retreive data in tmp directory
 if [[ "${checkout}" == "${pipeline}" ]]
@@ -46,9 +47,64 @@ eval ${var::-1} || {
 }
 echo "DONE with pulling/processing, now starting the pipeline"
 module load NGS_Automated
-module load "${pipeline}/betaAutotest"
+module load ${pipeline}/betaAutotest
 
 startNextflowDragenPipeline.sh -g umcg-gst
 
+mkdir -p "${TEMP}"
+## Some tests to see whether the pipeline ran successfully
+## check if captured file exists
+if [[ ! -f "${TMPDIRECTORY}/projects/NGS_DNA/${PROJECT}/run01/results/variants/1111111_123456_HG001_0000000_GS001A_WGS_000001_12348765.captured.vcf.gz" ]]
+then
+	echo "variants/1111111_123456_HG001_0000000_GS001A_WGS_000001_12348765.captured.vcf.gz does not exist"
+	exit 1
 fi
+#check if concordanceCheck made the correct calls
+if [[ ! -f "${TMPDIRECTORY}/projects/NGS_DNA/${PROJECT}/run01/results/concordanceCheckSnps/1111111_123456_HG001_0000000_GS001A_WGS_000001_12348765.concordanceCheckCalls.vcf" ]]
+then
+	echo "1111111_123456_HG001_0000000_GS001A_WGS_000001_12348765.concordanceCheckCalls.vcf does not exist"
+	exit 1
+else
+	## check if the variants are called
+	grep -v '^#' "${TMPDIRECTORY}/projects/NGS_DNA/${PROJECT}/run01/results/concordanceCheckSnps/1111111_123456_HG001_0000000_GS001A_WGS_000001_12348765.concordanceCheckCalls.vcf" > "${TEMP}/concordanceCheckCalls.vcf"
+	diffInConcordance='no'
+	diff -q "${WORKDIR}/test/trueConcordanceCheckCalls.vcf" "${TEMP}/concordanceCheckCalls.vcf" || diffInConcordance='yes'
+
+	if [[ "${diffInConcordance}" == 'yes' ]]
+	then
+		echo "There are some differences in the concordanceCheckCalls.vcf file"
+		echo "TRUE:"
+		cat "${WORKDIR}/test/trueConcordanceCheckCalls.vcf"
+		echo -e "\n\n NEW FILE:"
+		cat "${TEMP}/concordanceCheckCalls.vcf"
+	fi
+fi
+
+# check if .seg.bw file is there
+if [[ ! -f "${TMPDIRECTORY}/projects/NGS_DNA/${PROJECT}/run01/results/variants/cnv/1111111_123456_HG001_0000000_GS001A_WGS_000001_12348765.seg.bw" ]]
+then
+    	echo "variants/cnv/1111111_123456_HG001_0000000_GS001A_WGS_000001_12348765.seg.bw does not exist"
+        exit 1
+fi
+
+# check if stats file column is converted correctly
+if [[ ! -f "${TMPDIRECTORY}/projects/NGS_DNA/${PROJECT}/run01/results/qc/stats.tsv" ]]
+then
+    	echo "qc/stats.tsv does not exist"
+        exit 1
+else
+	if [[ $(awk '{ if (NR>1){ print $1} }' "${TMPDIRECTORY}/projects/NGS_DNA/${PROJECT}/run01/results/qc/stats.tsv") != '1111111_123456_HG001_0000000_GS001A_WGS_000001_12348765' ]]
+	then
+		echo "renaming is not done properly!"
+		echo -e "The first column of ${TMPDIRECTORY}/projects/NGS_DNA/${PROJECT}/run01/results/qc/stats.tsv should have been:\n1111111_123456_HG001_0000000_GS001A_WGS_000001_12348765, but it is:["
+		awk '{if (NR>1){print $1}}' "${TMPDIRECTORY}/projects/NGS_DNA/${PROJECT}/run01/results/qc/stats.tsv"
+		echo "]"
+	fi
+fi
+
+
+echo -e "\n Test succeeded!!\n"
+fi
+
+
 
